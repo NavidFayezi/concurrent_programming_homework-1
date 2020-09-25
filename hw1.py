4ed383b821c93a542f549a908045304b688a0193
NavidFayezi <navidfayezi.98@gmail.com>
2020-09-26 00:02:43 +0330

    final

--- hw1.py
+++ hw1.py
@@ -1,23 +1,42 @@
 import random
+import threading
 
 
-def file_initializer():
+def file_initializer():                 # creates 10 file each containing 100 random integers
     name_string = "file"
     for i in range(10):
         file_name = name_string + str(i) + ".txt"
         my_file = open(file_name, 'w')
         for j in range(100):
             my_file.write(str(random.randint(0, 100)) + '\n')
+        my_file.close()
 
 
-def read(filename):
+def read(filename):                     # reads the numbers in a file and stores them in a list
     numbers = []
     file = open(filename)
     for lines in file:
         numbers.append(int(file.readline()))
+    file.close()
     return numbers
 
+
+def sortnwrite(filename):               # writes the sorted numbers back to the file
+    numbers = read(filename)
+    numbers.sort()
+    my_file = open(filename, 'w')
+    for number in numbers:
+        my_file.write(str(number)+'\n')
+    my_file.close()
+
+
 if __name__ == "__main__":
-    #file_initializer()
-    print(read("file0.txt"))
+    # file_initializer()
+    threads = []
+    for i in range(10):
+        my_thread = threading.Thread(target=sortnwrite, args=("file"+str(i)+".txt",))
+        my_thread.start()
+        threads.append(my_thread)
+    for i in range(10):
+        threads[i].join()
 
